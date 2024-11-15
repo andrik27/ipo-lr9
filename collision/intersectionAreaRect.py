8a84ec5688e8ff6f8ba77f335e893df75313f6cb
Endru <88160476+andrik27@users.noreply.github.com>
2024-11-15 15:31:07 +0300

    Task 4 complete

--- collision/intersectionAreaRect.py
+++ collision/intersectionAreaRect.py
@@ -32,25 +32,3 @@ def intersectionAreaRect(rect1, rect2):
     y_overlap = max(0, min(rect1[1][1], rect2[1][1]) - max(rect1[0][1], rect2[0][1]))
 
     return x_overlap * y_overlap
-
-# Функция для ввода координат с клавиатуры
-def input_coordinates():
-    pointX1 = float(input('Введите координаты левого нижнего угла по иксу: '))
-    pointY1 = float(input('Введите координаты левого нижнего угла по игрику: '))
-    pointX2 = float(input('Введите координаты правого верхнего угла по иксу: '))
-    pointY2 = float(input('Введите координаты правого верхнего угла по игрику: '))
-    
-    return [(pointX1, pointY1), (pointX2, pointY2)]
-
-# Ввод координат для двух прямоугольников
-print("Введите координаты первого прямоугольника:")
-rect1 = input_coordinates()
-print("Введите координаты второго прямоугольника:")
-rect2 = input_coordinates()
-
-# Проверка пересечения прямоугольников и вычисление площади пересечения
-try:
-    Area = intersectionAreaRect(rect1, rect2)
-    print(f"Площадь пересечения: {Area}")
-except RectCorrectError as e:
-    print(e)

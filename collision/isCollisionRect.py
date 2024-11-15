0141c6011b95f5dc94080175d02716d09668e121
Endru <88160476+andrik27@users.noreply.github.com>
2024-11-15 15:33:11 +0300

    Task 3 complete

--- collision/isCollisionRect.py
+++ collision/isCollisionRect.py
@@ -19,27 +19,3 @@ def isCollisionRect(rects):
         return False
     return True
 
-# Функция для ввода координат с клавиатуры
-def input_coordinates():
-    pointX1 = float(input('Введите координаты левого нижнего угла по иксу: '))
-    pointY1 = float(input('Введите координаты левого нижнего угла по игрику: '))
-    pointX2 = float(input('Введите координаты правого верхнего угла по иксу: '))
-    pointY2 = float(input('Введите координаты правого верхнего угла по игрику: '))
-    
-    return [(pointX1, pointY1), (pointX2, pointY2)]
-
-# Ввод координат для двух прямоугольников
-print("Введите координаты первого прямоугольника:")
-rect1 = input_coordinates()
-print("Введите координаты второго прямоугольника:")
-rect2 = input_coordinates()
-
-# Объединяем прямоугольники в список списков
-rects = [rect1, rect2]
-rects = tuple(rects)
-# Проверка пересечения прямоугольников
-try:
-    result = isCollisionRect(rects)
-    print(f"Пересекаются ли прямоугольники? {result}")
-except RectCorrectError as e:
-    print(e)

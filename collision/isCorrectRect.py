90da6380bfb9d1b17c90acfee6676ab2a2ddbca1
Endru <88160476+andrik27@users.noreply.github.com>
2024-11-16 23:45:53 +0300

    Task 2 complete

--- collision/isCorrectRect.py
+++ collision/isCorrectRect.py
@@ -1,5 +1,2 @@
-def isCorrectRect(point1,point2):
-    flag = False
-    if(point1[0] < point2[0] and point1[1] < point2[1]):
-                flag = True
-    return print(flag)
+def isCorrectRect(rect):
+    return rect[0][0] < rect[1][0] and rect[0][1] < rect[1][1]
